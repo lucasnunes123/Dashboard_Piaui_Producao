7f0f7ec39e7f4d299334a62dc0234648c8f5c5b7
Lucas Nunes <lukasnunesgs@gmail.com>
2025-05-08 10:05:47 -0300

    Add volta o cao arrependido

--- pages/Documentos.py
+++ pages/Documentos.py
@@ -14,15 +14,6 @@ with open("assets/boletim.pdf", "rb") as f:
 
 st.image("assets/bol2.jpg", width=300)
 
-st.markdown(
-    """
-    <div style="text-align: left;">
-        <img src="http://localhost:8501/media/5e6ca2d8904aa2de2697fabfa2efb2caf2d956b01615ac3f5d2a48f9.jpg" width="300">
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
 st.download_button(label="📄 Baixar PDF",
                    data=pdf_data,
                    file_name="meuarquivo.pdf",
