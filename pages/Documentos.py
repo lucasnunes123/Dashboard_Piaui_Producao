378f206abc78184a1cc6519523f749b01f84958d
Lucas Nunes <lukasnunesgs@gmail.com>
2025-05-08 09:43:18 -0300

    update pagina documentos

--- pages/Documentos.py
+++ pages/Documentos.py
@@ -7,9 +7,21 @@ st.title("Boletim Comercial Trimestral")
 with open("assets/boletim.pdf", "rb") as f:
     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
+
+
+with open("assets/boletim.pdf", "rb") as f:
+    pdf_data = f.read()
+
+st.image("assets/bol.png", width=300)
+
+st.download_button(label="📄 Baixar PDF",
+                   data=pdf_data,
+                   file_name="meuarquivo.pdf",
+                   mime="application/pdf")
+
 # Exibe o PDF
 pdf_display = f"""
-<div style="display: flex; justify-content: center;">
+<div style="display: flex; justify-content: left;">
     <iframe src="data:application/pdf;base64,{base64_pdf}" 
             width="900" height="700" type="application/pdf">
     </iframe>
@@ -17,12 +29,3 @@ pdf_display = f"""
 """
 
 st.markdown(pdf_display, unsafe_allow_html=True)
-
-
-with open("assets/boletim.pdf", "rb") as f:
-    pdf_data = f.read()
-
-st.download_button(label="📄 Baixar PDF",
-                   data=pdf_data,
-                   file_name="meuarquivo.pdf",
-                   mime="application/pdf")
