6f287eb0d9537b50231662fea0dcdc92f46fa279
Lucas Nunes <lukasnunesgs@gmail.com>
2025-07-22 11:29:48 -0300

    updade

--- pages/Documentos.py
+++ pages/Documentos.py
@@ -18,14 +18,3 @@ st.download_button(label="📄 Baixar PDF",
                    data=pdf_data,
                    file_name="meuarquivo.pdf",
                    mime="application/pdf")
-
-# Exibe o PDF
-pdf_display = f"""
-<div style="display: flex; justify-content: left;">
-    <iframe src="data:application/pdf;base64,{base64_pdf}" 
-            width="900" height="700" type="application/pdf">
-    </iframe>
-</div>
-"""
-
-st.markdown(pdf_display, unsafe_allow_html=True)
